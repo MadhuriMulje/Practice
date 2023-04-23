9ae27da231e65b9fe6bdf2c66596531295b956f6
Madhuri Mulje <madhurim@synerzipune.local>
2023-04-23 18:29:31 +0530

    c sharp code

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace A
+{
+    class calci
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * double n1, n2, res;
+            char op;
+            for(int i = 0; i < 5; i++) { 
+            Console.WriteLine("enter number1-> ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter number2-> ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" enter Uperator(+,-,*,/)");
+            op = Convert.ToChar(Console.ReadLine());
+                switch (op)
+                {
+
+                    case '+':
+                        res = n1 + n2;
+                        Console.WriteLine("{0} + {1} = {2}", n1, n2, res);
+                        break;
+                    case '-':
+                        res = n1 - n2;
+                        Console.WriteLine("{0} - {1} = {2}", n1, n2, res);
+                        break;
+                }*/
+
+
+
+
+           /* for (int i = 1, prevFib = 1, curFib = 1; i < 20; i++)
+            {
+                Console.WriteLine(prevFib);
+                int newFib = prevFib + curFib;
+                prevFib = curFib;
+                curFib = newFib;
+                Console.WriteLine(curFib);
+            }*/
+
+        }
+
+        }
+    }
