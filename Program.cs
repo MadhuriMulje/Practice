f1c6ad0463b992e739d62e7678a6ca23d1f3cbee
MadhuriMulje <131591916+MadhuriMulje@users.noreply.github.com>
2023-04-23 18:33:54 +0530

    switch

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace A
+{
+    class Aswitch
+
+    {
+        static void Main(string[] args)
+        {
+            char ch;int i = 1;
+            for (i=1; i < 5; i++) {
+
+                Console.WriteLine(" Enter cahracter -> ");
+
+                ch = Convert.ToChar(Console.ReadLine());
+                switch (char.ToLower(ch))
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        Console.WriteLine("vovels" + ch);
+                        break;
+                    default:
+                        Console.WriteLine("not vovels");
+                        break;
+                }
+
+            }
+           
+        }
+    }
+}
+   
